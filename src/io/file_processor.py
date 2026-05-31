7ac1b7f026fc6997ae90484beff0752a356f82e0
Tadeus <99.twister@gmail.com>
2026-05-31 13:48:04 +0200

    remove left-over code

--- src/io/file_processor.py
+++ src/io/file_processor.py
@@ -103,11 +103,3 @@ class FileProcessor:
 				errors.append(f"Linia {i}: {str(e)}")
 
 		return dataset, errors, metadata
-
-	def save_txt(self, path, content):
-			with open(path, "w", encoding="utf-8") as f:
-					f.write(content)
-
-	def save_json(self, path, data):
-			with open(path, "w", encoding="utf-8") as f:
-					json.dump(data, f, indent=2, ensure_ascii=False)
